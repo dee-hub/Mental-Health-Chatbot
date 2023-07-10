155c370dd2abc8cea4c313e7f2baafdb56b0ab28
Salako Oluwadolapo <willdynamics@gmail.com>
2023-07-10 12:21:53 +0100

    Update chatbot_interface.py

--- chatbot_interface.py
+++ chatbot_interface.py
@@ -23,6 +23,11 @@ from torch.utils.data import Dataset, DataLoader
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 from nltk.stem import WordNetLemmatizer
+import openai
+import streamlit as st
+# pip install streamlit-chat
+from streamlit_chat import message
+
 lemmatizer = WordNetLemmatizer()
 
 def tokenize(sentence):
@@ -133,72 +138,6 @@ hidden_size = 8
 output_size = len(tags)
 print(input_size, output_size)
 
-class ChatDataset(Dataset):
-
-    def __init__(self):
-        self.n_samples = len(X_train)
-        self.x_data = X_train
-        self.y_data = y_train
-
-    # support indexing such that dataset[i] can be used to get i-th sample
-    def __getitem__(self, index):
-        return self.x_data[index], self.y_data[index]
-
-    # we can call len(dataset) to return the size
-    def __len__(self):
-        return self.n_samples
-
-dataset = ChatDataset()
-train_loader = DataLoader(dataset=dataset,
-                          batch_size=batch_size,
-                          shuffle=True,
-                          num_workers=0)
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-model = NeuralNet(input_size, hidden_size, output_size).to(device)
-
-# Loss and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-# Train the model
-for epoch in range(num_epochs):
-    for (words, labels) in train_loader:
-        words = words.to(device)
-        labels = labels.to(dtype=torch.long).to(device)
-
-        # Forward pass
-        outputs = model(words)
-        # if y would be one-hot, we must apply
-        # labels = torch.max(labels, 1)[1]
-        loss = criterion(outputs, labels)
-
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-    if (epoch+1) % 100 == 0:
-        print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
-
-
-print(f'final loss: {loss.item():.4f}')
-
-data = {
-"model_state": model.state_dict(),
-"input_size": input_size,
-"hidden_size": hidden_size,
-"output_size": output_size,
-"all_words": all_words,
-"tags": tags
-}
-
-FILE = "bot.pth"
-torch.save(data, FILE)
-
-print(f'training complete. file saved to {FILE}')
-
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 with open('intents.json', 'r') as json_data:
@@ -242,14 +181,6 @@ def get_response(msg):
         return "I do not understand..."
 
 #Type your questions within the functions
-
-import openai
-import streamlit as st
-
-# pip install streamlit-chat
-from streamlit_chat import message
-
-
 
 #Creating the chatbot interface
 st.title("chatBot : Streamlit + openAI")
