c7deb8abf12348403f862cb4b1d969bd21558d78
Salako Oluwadolapo <willdynamics@gmail.com>
2023-07-10 15:24:09 +0100

    Update chatbot_interface.py

--- chatbot_interface.py
+++ chatbot_interface.py
@@ -173,7 +173,7 @@ def get_response(msg):
 
     probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
-    if prob.item() > 0.75:
+    if prob.item() > 0.85:
         for intent in intents['intents']:
             if tag == intent["tag"]:
                 print(f"Identified context = {tag}")
