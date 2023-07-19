ae7f5839cbc5635d019f68bf371be28a497f0d0b
Salako Oluwadolapo <willdynamics@gmail.com>
2023-07-19 08:32:59 +0100

    Update chatbot_interface.py

--- chatbot_interface.py
+++ chatbot_interface.py
@@ -92,10 +92,10 @@ with st.sidebar:
     or simply seeking someone to talk to, HUSU is here to listen and offer personalized guidance. \
     By utilizing cutting-edge natural language processing, HUSU can comprehend and respond to a wide range of \
     emotions and concerns, tailoring its responses to each user's unique needs. \n\n It can also provide helpful coping mechanisms, \
-    relaxation techniques, and resources to promote self-care and emotional resilience. \n\n Additionally, HUSU respects your privacy and confidentiality,\
+    relaxation techniques, and resources to promote self-care and emotional resilience. \n\n Additionally, HUSU respects your privacy and confidentiality, \
     ensuring that all interactions remain secure and anonymous. Its primary goal is to \
-    assist and support users in their mental health journey, acting as a complement to professional counseling rather than a replacement. \n\n  \
-    Remember, seeking help and discussing your emotions is an essential step towards mental well-being, \
+    assist and support users in their mental health journey, acting as a complement to professional counseling rather than a replacement. \
+    \n\n Remember, seeking help and discussing your emotions is an essential step towards mental well-being, \
     and HUSU is here to be your caring companion on this path to a healthier and happier life. \
     Don't hesitate to reach out to HUSU whenever you need someone to talk to or require assistance \
     with managing your mental health. Together, we can work towards a brighter and more fulfilling future.""")
